a00124cfe51de71638c95bcd8e512e514e580b01
Omar Omeiri <omaromeiri@gmail.com>
2023-06-06 19:12:58 -0300

    removed useless home link

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,8 @@
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import { fetchCoordinates } from '../../Http/requests/aerodrome';
 import Config from '../../config';
 import RotaerLoadingSpinner from '../../components/Loading/RotaerLoadingSpinner';
-import { APP_ROUTES } from '../../consts/routes';
 
 const { height: navBarHeight } = Config.get('styles').navBar;
 
@@ -23,7 +21,6 @@ export default async function Home() {
   const coords = await fetchCoordinates(undefined);
   return (
     <div style={{ height: `calc(var(--vh) - ${navBarHeight + 1}px)`, display: 'flex', flexDirection: 'column' }}>
-      <Link href={APP_ROUTES.myAircraft('pt-BR')}>KKEKEKEKEK</Link>
       <div style={{ flexGrow: '1' }}>
         <Suspense fallback={<LoadingSpinner/>}>
           <GMap markers={coords}/>
